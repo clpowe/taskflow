729670d604f43c35d287e3dff2897411875b0e13
clpowe <clpowe@gmail.com>
2026-08-26 15:25:13 -0400

    added controller

--- app/actions/controller.tsx
+++ app/actions/controller.tsx
@@ -1,18 +1,27 @@
 import { createController } from 'remix/router'
-
 import { assetServer } from '../assets.ts'
+import { getDashboard } from '../data/tasks.ts'
 import { routes } from '../routes.ts'
-import { HomePage } from './home-page.tsx'
+import { HomePage } from './home.tsx'
 
 export default createController(routes, {
   actions: {
     async assets(context) {
       return (
-        (await assetServer.fetch(context.request)) ?? new Response('Not Found', { status: 404 })
+        (await assetServer.fetch(context.request)) ??
+        new Response('Not Found', {
+          status: 404,
+        })
       )
     },
-    home(context) {
-      return context.render(<HomePage />)
+
+    async home(context) {
+      const data = await getDashboard(context.db)
+      return context.render(<HomePage data={data} />, {
+        headers: {
+          'Cache-Control': 'no-store',
+        },
+      })
     },
   },
 })
